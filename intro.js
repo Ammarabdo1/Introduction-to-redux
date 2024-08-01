12d265ef003bc6ad029e42933ca9c274e5a24e18
AmmarAbdo002 <ammarabbdo.002@gmail.com>
2024-08-01 12:11:27 +0300

    first commit

---
+++ intro.js
@@ -0,0 +1,13 @@
+//? Hints 
+
+//TODO>> 1)React is a library used to build user interfaces.
+
+//TODO>>  2)Redux is a library for managing state in a predictable way in JavaScript applications.
+
+//TODO>> 3)Redux in not tied to React but can be used with React , angular ,Vue.
+
+//TODO>> 4)React-redux is a library that provides bindings to use React and Redux together in an application.
+
+//TODO>> 5)Redux stores the state of your application.
+
+//TODO>> 6)React context - prevents prop drilling.
